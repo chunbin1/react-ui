75e9b00ccb13d341744bdd36c751f108775f131c
Lee <545360100@qq.com>
2019-03-11 23:40:29 +0800

    Updates

---
+++ precache-manifest.ee05b9cb1f04b378ad85126beae68d8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "12c26ccd3c6a174b5b51",
+    "url": "/react-ui/static/css/main.8dff3295.chunk.css"
+  },
+  {
+    "revision": "12c26ccd3c6a174b5b51",
+    "url": "/react-ui/static/js/main.8590cd71.chunk.js"
+  },
+  {
+    "revision": "eb14ab42b6964c19a45e",
+    "url": "/react-ui/static/js/runtime~main.b6e99655.js"
+  },
+  {
+    "revision": "50a4069f35415aacbed2",
+    "url": "/react-ui/static/js/2.b96e73ca.chunk.js"
+  },
+  {
+    "revision": "3a2c5cc52f1981cc29ecc8805d99b308",
+    "url": "/react-ui/static/media/1.3a2c5cc5.jpg"
+  },
+  {
+    "revision": "94d6aaee296cba08bd42833d6aa1ae37",
+    "url": "/react-ui/static/media/2.94d6aaee.jpg"
+  },
+  {
+    "revision": "07ab7d80f85d0b77496d17a8b7769907",
+    "url": "/react-ui/static/media/3.07ab7d80.jpg"
+  },
+  {
+    "revision": "2abd5f0547d1dcb8e1d1f544f8f0b80e",
+    "url": "/react-ui/static/media/4.2abd5f05.jpg"
+  },
+  {
+    "revision": "7420ced962ac54976a54d783bc61b2ae",
+    "url": "/react-ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.420f4f7b254dc056da0b32079c7e80bb.js"
+  "/react-ui/precache-manifest.ee05b9cb1f04b378ad85126beae68d8c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-ui/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
