740269553e241c6a23dfb129f3d1aa8e02de9e3a
Lee <545360100@qq.com>
2019-03-11 11:47:16 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+import Carousel from './components/Carousel/Carousel'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+         <p>无限轮播</p>
+         <Carousel>
+           
+         </Carousel>
       </div>
+     
     );
   }
 }
