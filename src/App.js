8c0b1bf1dd7753722a8e2a8942e67b9d1335b10b
simons <licb@mogulinker.com>
2019-03-22 17:28:31 +0800

    Tabs组件1.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 
 import Carousel from "./components/Carousel/Carousel";
-import ScssTest from './components/ScssTest/Scsstest'
+import Tabs from "./components/Tabs/Tabs";
+import ScssTest from "./components/ScssTest/Scsstest";
 import "./App.css";
 
 const requireContext = require.context("./asserts/img", true, /^\.\/.*\.jpg$/);
 const img = requireContext.keys().map(requireContext);
+const TabPane = Tabs.TabPane;
+
 class App extends Component {
   render() {
     return (
       <div>
         <p>无限轮播</p>
-        <Carousel width={450} height={300} >
+        <Carousel width={450} height={300}>
           {img.map((val, idx) => (
             <img
               style={{ display: "inline-block" }}
@@ -21,11 +24,20 @@ class App extends Component {
             />
           ))}
         </Carousel>
-
+        <p>Tabs</p>
+        <Tabs>
+          <TabPane tab="Tab 1" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
         <p>scss的测试</p>
-        <ScssTest>
-          
-        </ScssTest>
+        <ScssTest />
       </div>
     );
   }
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import className from 'classnames'
+import './Tabs.scss'
+
+const headerStyle = {
+  width:'100px',
+  height:'20px',
+  border:'1px solid black',
+  display:'inline-block',
+}
+
+export default class Tabs extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      index:this.props.defaultKey||0,
+    }
+    this.renderTabs = this.renderTabs.bind(this)
+    this.clickTabs = this.clickTabs.bind(this)
+    this.renderContent = this.renderContent.bind(this)
+  }
+
+  renderTabs(){
+    const {children} = this.props
+    return React.Children.map(children,(child,index) => {
+      const childClone = React.cloneElement(child)
+      return (
+        <div className={className({'tab-selected':this.state.index===index})} key={index} index={index} onClick={this.clickTabs} style={headerStyle}>
+          {childClone.props.tab}
+        </div>
+      )
+    }
+    )
+  }
+
+  clickTabs(e){
+    const index = Number(e.target.getAttribute('index'))
+    this.setState({index,})
+  }
+
+  renderContent(){
+    const {children} = this.props
+    return React.Children.map(children,(child,index)=>{
+      const childClone = React.cloneElement(child)
+      const {children} = childClone.props
+      return <div style={{display:this.state.index===index?'block':'none',width:"100%"}} >{children}</div>
+    })
+  } 
+
+  render() {
+    return (
+      <div style={{position:"relative"}}>
+        <div className='tabs-header'>
+          {this.renderTabs()}
+        </div>
+        <div className='tabs-content'>
+          {this.renderContent()}
+        </div>
+      </div>
+    )
+  }
+}
+
+function TabPane(props) { 
+  return <div {...props}></div>
+}
+
+Tabs.TabPane  = TabPane
